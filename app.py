2681889f2d9f7a1a97dad5f7780fcd1c4a1480af
ruruyin-png <rushilrawat6072@gmail.com>
2024-11-02 22:23:56 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import request
+
+app = Flsk(__name__)
+
+@app.route("/")
+def visitors():
+
+    counter_read_file = open("count.txt", "r")
+    visitors_count = int(counter_read_file.read())
+    counter_read_file.close()
+
+    visitors_count = visitors_count + 1
+
+    counter_write_file = open("count.txt", "w")
+    counter_write_file.write(str(visitors_count))
+    counter_write_file.close()
+
+    return render_template("index.html", count=visitors_count)
+
+@app.route('/',method=['POST'])
+def weather_stats():
+    
+    counter_read_file = open("count.txt", "r")
+    visitors_count = int(counter_read_file.read())
+    counter_read_file.close()
+
+    latitude = request.form['latitude']
+    longitude = request.form['longitude']
+
+    api_url = 'https://weather-l6tl.onrender.com/api/getCurrentWeather/31/76'
+
+    response = requests.get(api_url)
+    weather_data = response.json()
+    print(weather_data)
+    return render_template("index.html", weather=weather_data, count=visitors_count)
+
+if __name__ == "__main__":
+    app.run()
